337de920febe2c9e0231f7b4651acb02eac9f1e0
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-15 14:13:19 +0800

    added • to sidebar when active

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,12 +23,14 @@ export default function Sidebar() {
 
   const handleClick = (index, route) => {
     navigate(route);
-    setActive(index)
+    setActive(index);
   };
   return (
     <aside
       id="sidebar"
-      className="w-[18vw] h-screen overflow-hidden bg-secondary-dark-blue relative border-r-4 border-r-secondary-dark-blue flex flex-col items-center"
+      className="w-[18vw] h-screen overflow-hidden bg-secondary-dark-blue relative border-r-4
+       border-r-secondary-dark-blue flex flex-col items-center 
+       transition-all duration-300"
     >
       <div id="logo" className="bg-white pt-4">
         <img src={SidebarLogo} alt="" />
@@ -40,32 +42,42 @@ export default function Sidebar() {
         {SidebarButtons.map((item, index) => (
           <button
             className={`${
-              index === active ? "contast-125 brightness-50" : ""
-            } flex flex-row items-center w-8/10 hover:brightness-50 hover:contrast-125`}
+              index === active
+                ? "contast-125 brightness-50 after:content-[.]"
+                : ""
+            } flex flex-row items-center w-8/10 hover:brightness-50 hover:contrast-125 relative`}
             key={index}
             type="button"
-            onClick= {() => handleClick(index, item.route)}
+            onClick={() => handleClick(index, item.route)}
           >
             <img src={item.icon} className="w-2/10 mr-5" />
             <p className="w-5/10 pb-3 text-text-content text-center border-b-3 border-t-white font-heading">
               {item.text}
             </p>
+            <p
+              className={`${
+                index === active ? "block" : "hidden"
+              } absolute text-white contrast-125 right-2 text-2xl`}
+            >
+              •
+            </p>
           </button>
         ))}
       </div>
+
       <div
         id="design-line"
         className="h-fit w-full absolute top-[20vmin] lg:block sm:hidden"
       >
-        <div className="bg-white w-[50%] h-2"></div>
-        <div className="bg-transparent w-[45%] h-2 border-b-2 border-r-2 border-white"></div>
+        <div className="bg-white w-[50%] h-1"></div>
+        <div className="bg-transparent w-[45%] h-1 border-b-2 border-r-2 border-white"></div>
       </div>
       <div
         id="design-line"
         className="h-fit w-full absolute bottom-[calc(17vw*0.45)] left-[14vmin] rotate-270"
       >
-        <div className="bg-white w-[50%] h-2"></div>
-        <div className="bg-transparent w-[45%] h-2 border-b-2 border-r-2 border-white lg:block sm:hidden"></div>
+        <div className="bg-white w-[50%] h-1"></div>
+        <div className="bg-transparent w-[45%] h-1 border-b-2 border-r-2 border-white lg:block sm:hidden"></div>
       </div>
     </aside>
   );
