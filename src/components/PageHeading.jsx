e7b30d177e14f3935a85666f9c71c4170d12c920
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-15 13:55:36 +0800

    Built a reusable pageheading component

---
+++ src/components/PageHeading.jsx
@@ -0,0 +1,9 @@
+export default function PageHeading({ text }) {
+  return (
+    <div id="page-heading" className="h-[25%] flex justify-center items-center flex-col border-b-2 border-black">
+      <h1 className="font-heading text-5xl text-primary-blue">{text}</h1>
+      <hr className="w-[20%] border-1 border-primary-yellow m-3"/>
+      <p className="font-heading text-2xl">Power of Physical Education</p>
+    </div>
+  );
+}
