b8c9b548a913555227027464949a142f0796ed50
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-15 11:26:05 +0800

    Removed app contents, fixed font issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { Button } from "../src/components/ui/button"
-import "./App.css";
+import "./styles/global.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div className="p-4">
-      <h1 className="text-2xl font-semibold mb-4">ShadCN UI + React + Tailwind CSS</h1>
-      <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        ShadCN Button
-      </Button>
-    </div>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
+// filepath: c:\Users\user\Documents\projects\Commisions\hope-hub\vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";  
-// https://vite.dev/config/
+import tailwind from "@tailwind";
+import postcssTailwind from "@tailwindcss/postcss";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwind()],
+  css: {
+    postcss: {
+      plugins: [postcssTailwind()],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
