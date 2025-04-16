bdbf80d90fb2b481f9461fa22cece382f31d71cd
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-16 12:30:33 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Sidebar from "./components/Sidebar";
 import About from "./pages/About";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
-//CONTENT COMPONENTS SHOULD BE 100VW-18VW WIDE
-//SIDEBAR SIZE IS 18VW
 
 function App() {
   const SideBarOutlet = () => {
