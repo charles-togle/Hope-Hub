edb00f2becc5795769e5176faaf57ad572a324a2
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-16 22:19:35 +0800

    added index prop

--- src/components/LectureIntroduction.jsx
+++ src/components/LectureIntroduction.jsx
@@ -1,7 +1,8 @@
-export default function LectureIntroduction({title, introduction, status}){
+export default function LectureIntroduction({index, title, introduction, status}){
     console.log(title)
     return(
-        <div id="lecture-introduction">
+        <div id="lecture-introduction" className="border-3 border-black rounded-md">
+            <p>{index}</p>
             <p>{title}</p>
             <p>{introduction}</p>
             <p>{status}</p>
