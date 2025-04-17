fefc2955e95e5890065a3ca9b0804add6c3ae414
Charles Nathaniel Togle <149758871+charles-togle@users.noreply.github.com>
2025-04-17 17:23:43 +0800

    Built Lecture selection page (#4)
    * Created Lectures page, and added routing
    
    * Transferred Lessons to a different file
    
    * Created a component for Lecture Introduction
    
    * added an option to add designs for page heading
    
    * added more lectures
    
    * added index prop
    
    * Created Lectures page, missing: style for lecture introduction
    
    * Finished Lecture Introduction page

---
+++ src/utilities/Lessons.js
@@ -0,0 +1,56 @@
+export const Lessons = [
+  {
+    title: "This is a sample title",
+    introduction: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+      enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+      aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+      in voluptate velit esse cillum dolore eu fugiat nulla pariatur. `,
+    status: "Pending",
+  },
+  {
+    title: "This is a sample title",
+    introduction: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. `,
+    status: "Done",
+  },
+  {
+    title: "This is a sample title",
+    introduction: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. `,
+    status: "Pending",
+  },
+  {
+    title: "This is a sample title",
+    introduction: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. `,
+    status: "Incomplete",
+  },
+  {
+    title: "This is a sample title",
+    introduction: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. `,
+    status: "Pending",
+  },
+  {
+    title: "This is a sample title",
+    introduction: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. `,
+    status: "Pending",
+  },
+];
