ffc079cae1611b350ec4b827c9f839914252c3dc
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-17 18:07:01 +0800

    added sa sample lesson with pdf and video

--- src/utilities/Lessons.js
+++ src/utilities/Lessons.js
@@ -7,8 +7,8 @@ export const Lessons = [
       aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
       in voluptate velit esse cillum dolore eu fugiat nulla pariatur. `,
     status: "Pending",
-    pdf: "https://pdf.com",
-    videoLecture: "https://youtube.com",
+    pdf: "https://file.garden/Z-9MLal_RynAMoBV/PDFs/PERSONAL%20SAFETY%20PROTOCOLS_PDF.pdf",
+    videoLecture: "https://file.garden/Z-9MLal_RynAMoBV/Videos/Stock%20Video%201.mp4",
   },
   {
     title: "This is a sample title",
