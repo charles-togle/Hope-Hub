8302b9ebd35f43d8ecc61a14415272f8b9bb35ec
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-17 19:41:44 +0800

    added routing for lecture page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Sidebar from "./components/Sidebar";
 import About from "./pages/About";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Lectures from "./pages/Lectures";
+import LecturePage from "./pages/LecturePage";
 
 function App() {
   const SideBarOutlet = () => {
     return (
       <div className="flex h-screen overflow-hidden">
         <Sidebar />
-        <div className="flex-1 h-screen overflow-auto overflow-x-hidden justify-center">
+        <div className="flex-1 h-screen overflow-y-scroll overflow-x-hidden justify-center">
           <Outlet />
         </div>
       </div>
@@ -22,6 +23,10 @@ function App() {
         <Route element={<SideBarOutlet />} path="/">
           <Route path="about" element={<About />}></Route>
           <Route path="lectures" element={<Lectures />}></Route>
+          <Route
+            path="lectures/lecture/:lessonNumber"
+            element={<LecturePage />}
+          ></Route>
         </Route>
       </Routes>
     </BrowserRouter>
