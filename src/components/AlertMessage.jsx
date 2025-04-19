6f605c60a7b07232bf4b01aaf1bf5538d4d16581
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-19 21:39:31 +0800

    Updated the alert component UI

--- src/components/AlertMessage.jsx
+++ src/components/AlertMessage.jsx
@@ -14,21 +14,23 @@ export function AlertMessage({
       ></div>
       <div
         id="modal-content"
-        className="bg-secondary-dark-blue w-[50%] h-[40%] z-2 flex justify-center items-center relative p-20 text-center font-semibold"
+        className="bg-secondary-dark-blue w-[45%] h-[40%] z-2 flex flex-col justify-start items-center relative p-10 text-center rounded-md drop-shadow-sm"
       >
+        <p className="text-white text-2xl font-semibold">WARNING</p>
+        <hr className="w-full mb-5" />
         <p className="text-white mb-15 text-wrap">{text}</p>
         <div
           id="buttons"
           className="absolute bottom-10 right-10 flex space-x-5"
         >
           <button
-            className="px-7 py-2 bg-primary-yellow text-black hover:brightness-90"
+            className="px-5 py-1 border-1 border-primary-yellow text-primary-yellow hover:brightness-80"
             onClick={() => onCancel}
           >
             Cancel
           </button>
           <button
-            className="px-7 py-2 bg-white text-black hover:brightness-90"
+            className="px-5 py-1 border-1 border-primary-yellow bg-primary-yellow text-black hover:brightness-80"
             onClick={() => onConfirm}
           >
             Confirm
