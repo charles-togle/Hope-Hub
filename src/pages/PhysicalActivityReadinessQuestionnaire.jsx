b5dcefbbe2a85f8ce03ac3f30de134f6ac81c47c
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-21 21:55:37 +0800

    Update PhysicalActivityReadinessQuestionnaire.jsx

--- src/pages/PhysicalActivityReadinessQuestionnaire.jsx
+++ src/pages/PhysicalActivityReadinessQuestionnaire.jsx
@@ -2,7 +2,7 @@ import PageHeading from '@/components/PageHeading';
 import { usePhysicalFitnessData } from '@/hooks/usePhysicalFitnessData';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { AlertMessage } from '@/components/AlertMessage';
+import { AlertMessage } from '@/components/utilities/AlertMessage';
 
 export default function PhysicalActivityReadinessQuestionnaire() {
   const { physicalFitnessData, setPhysicalFitnessData } =
