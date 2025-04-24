b568aab3890c988b4067c916e061b3acf94f5732
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-24 23:20:58 +0800

    Added video tracking for video

--- src/pages/LecturePage.jsx
+++ src/pages/LecturePage.jsx
@@ -1,21 +1,44 @@
-import { Lessons } from "@/utilities/Lessons";
-import { useParams } from "react-router-dom";
-import PageHeading from "@/components/PageHeading";
-import { useState } from "react";
-import LecturePDF from "@/components/LecturePDF";
-import LectureVideo from "@/components/LectureVideo";
-import ErrorMessage from "@/components/utilities/ErrorMessage";
+import { Lessons } from '@/utilities/Lessons';
+import { useNavigate, useParams } from 'react-router-dom';
+import PageHeading from '@/components/PageHeading';
+import { useState, useEffect } from 'react';
+import LecturePDF from '@/components/LecturePDF';
+import LectureVideo from '@/components/LectureVideo';
+import ErrorMessage from '@/components/utilities/ErrorMessage';
 
 export default function LecturePage() {
-  const [isVideo, setIsVideo] = useState(false);
-  const { lessonNumber } = useParams();
+  const { lessonNumber, lectureType } = useParams();
+  const [isError, setIsError] = useState(false);
+  const [isVideo, setIsVideo] = useState(() => {
+    if (lectureType === 'video') {
+      return true;
+    } else if (lectureType === 'lecture') {
+      return false;
+    } else {
+      return false;
+    }
+  });
+  const navigate = useNavigate();
   const selectedLessonNumber = lessonNumber;
 
-  if (selectedLessonNumber > Lessons.length) {
-    return <ErrorMessage text={"Error 404"} subText={"Page not found"} />;
+  useEffect(() => {
+    if (lectureType !== 'video' && lectureType !== 'lecture') {
+      setIsError(true);
+    }
+  }, [lectureType]);
+
+  useEffect(()=>{
+    navigate(`/lectures/lecture/${selectedLessonNumber}/${isVideo ? "video" : "lecture"}`)
+  }, [isVideo, navigate, lectureType, selectedLessonNumber])
+
+
+  if (selectedLessonNumber > Lessons.length || isError) {
+    return <ErrorMessage text={'Error 404'} subText={'Page not found'} />;
   }
 
-  const lessonDetails = Lessons.find((lesson) => lesson.key === Number(selectedLessonNumber));
+  const lessonDetails = Lessons.find(
+    (lesson) => lesson.key === Number(selectedLessonNumber),
+  );
   const { pdf, introduction, title, videoLecture } = lessonDetails;
 
   return (
@@ -32,7 +55,7 @@ export default function LecturePage() {
           }}
           className="w-fit self-start px-5 mb-3 border-2 border-accent-blue py-2 text-xl font-content bg-white hover:bg-accent-blue hover:text-white transition-all"
         >
-          {isVideo ? "READ DOCUMENT" : "WATCH VIDEO LECTURE"}
+          {isVideo ? 'READ DOCUMENT' : 'WATCH VIDEO LECTURE'}
         </button>
         {isVideo ? (
           <LectureVideo
@@ -40,7 +63,7 @@ export default function LecturePage() {
             title={title}
             introduction={introduction}
             videoLink={videoLecture}
-            quizLink={""}
+            quizLink={''}
           />
         ) : (
           <LecturePDF
@@ -48,7 +71,7 @@ export default function LecturePage() {
             title={title}
             introduction={introduction}
             pdfLink={pdf}
-            quizLink={""}
+            quizLink={''}
           />
         )}
       </div>
