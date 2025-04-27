f5c67ece98998a1f1ebb887300c2c7de04c95333
Charles Nathaniel Togle <149758871+charles-togle@users.noreply.github.com>
2025-04-27 14:06:28 +0800

    added homepage and a placeholder (#24)

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div
+      id="home"
+      className="flex justify-center items-center w-full font-content h-full flex-col"
+    >
+      <h1 className="font-bold text-2xl">Home page</h1>
+      <p>This is the home page</p>
+    </div>
+  );
+}
