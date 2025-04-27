724fdc28efafcef6c6dbb653af3e85d2199cfce7
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-27 14:40:21 +0800

    Polished styling

--- src/pages/HealthCalculators/HealthCalculator.jsx
+++ src/pages/HealthCalculators/HealthCalculator.jsx
@@ -1,32 +1,53 @@
 import React from 'react';
 import PageHeading from '@/components/PageHeading';
-import CalculatorButton from '@/components/HealthCalculatorButtons';
+import HealthCalculatorButton from '@/components/HealthCalculatorButtons';
 import { Link } from 'react-router-dom';
 
 export default function HealthCalculator() {
-    const description = `Lorem ipsum dolor sit amet consectetur. Arcu vitae eget nunc in cras eget. Aliquam diam 
+  const description = `Lorem ipsum dolor sit amet consectetur. Arcu vitae eget nunc in cras eget. Aliquam diam 
     laoreet sed eget. Faucibus sed pretium risus viverra feugiat commodo nascetur. Eu quis 
     in mattis morbi velit habitant feugiat. Lorem ipsum dolor sit amet consectetur. Arcu vitae
     eget nunc in cras eget. Aliquam diam laoreet sed eget. Faucibus sed pretium risus viverra 
     feugiat commodo nascetur. Eu quis in mattis morbi velit habitant feugiat.`;
 
-    return (
-        <div id = "HealthCalculator"> 
-            <PageHeading text = "Health Calculators"></PageHeading>
-            <div className = " flex-col columns-2 m-16 gap-x-16">
-                <CalculatorButton text = "Body Mass Index (BMI) Calculator" > </CalculatorButton>
-                <CalculatorButton text = "Calorie Calculator" > </CalculatorButton>
-                <CalculatorButton text = "Basal Metabolic Rate (MBR) Calculator" > </CalculatorButton>
-                <CalculatorButton text = "Ideal Body Weight (IBW) Calculator" > </CalculatorButton>
-                <CalculatorButton text = "Body Fat Percentage Calculator" > </CalculatorButton>
-                <CalculatorButton text = "Daily Caloric Needs (TDEE) Calculator" > </CalculatorButton>
-                <CalculatorButton text = "Water Intake Calculator" > </CalculatorButton>
-            </div>
-            <hr className="border-1 border-primary-yellow yellow w-[43%] absolute right-0 mt-[-4rem]"></hr>
+  const HealthCalculatorButtons = [
+    { text: 'Body Mass Index (BMI) Calculator', className: '', linkTo: '' },
+    { text: 'Calorie Calculator', className: '', linkTo: '' },
+    { text: 'Calorie Calculator', className: '', linkTo: '' },
+    { text: 'Ideal Body Weight (IBW) Calculator', className: '', linkTo: '' },
+    { text: 'Body Fat Percentage Calculator', className: '', linkTo: '' },
+    {
+      text: 'Daily Caloric Needs (TDEE) Calculator',
+      className: '',
+      linkTo: '',
+    },
+    { text: 'Water Intake Calculator', className: '', linkTo: '' },
+  ];
 
-            <h1 className = "m-16 font-content text-4xl text-primary-blue"> Description </h1>
-            <hr className="border-1 border-primary-yellow yellow absolute left-16 w-[7%] mt-[-3.8rem]"></hr>
-            <p className='m-16 ml-30 font-content mt-[-2rem]'> {description} </p>
+  return (
+    <div id="health-calculator">
+      <PageHeading text="Health Calculators"></PageHeading>
+      <div
+        id="health-calculators-content"
+        className="w-[80%] flex justify-center flex-col items-center mr-auto ml-auto mt-16"
+      >
+        <div className="grid grid-cols-2 gap-y-3 gap-x-10 relative mb-10 w-full">
+          {HealthCalculatorButtons.map((button) => (
+            <HealthCalculatorButton
+              text={button.text}
+              linkTo={button.linkTo}
+              className={button.className}
+            ></HealthCalculatorButton>
+          ))}
+          <hr className="border-1 border-primary-yellow yellow w-[60%] absolute -right-55 bottom-0"></hr>
         </div>
-    );
-  }
+
+        <h1 className="font-content font-semibold text-3xl text-primary-blue self-start">
+          Description:{' '}
+        </h1>
+        <hr className="border-1 w-25 border-primary-yellow yellow self-start mt-1 mb-7"></hr>
+        <p className="font-content ml-10 mb-10"> {description} </p>
+      </div>
+    </div>
+  );
+}
