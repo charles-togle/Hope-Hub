ae1cba85504cf7c5e582b46999d11bf60ed974da
Charles Nathaniel Togle <149758871+charles-togle@users.noreply.github.com>
2025-05-10 21:38:27 +0800

    Backend/pft summary (#35)
    * added supabase env variables and spliited code in pft
    
    * retrieving pre test data from database
    
    * format

--- src/components/PhysicalFitnessTest.jsx
+++ src/components/PhysicalFitnessTest.jsx
@@ -7,6 +7,8 @@ import { AlertMessage } from './utilities/AlertMessage';
 import setDataToStorage from '@/utilities/setDataToStorage';
 import getDataFromStorage from '@/utilities/getDataFromStorage';
 import { Timer } from '@/components/utilities/Timer';
+import { ResultSection } from './subcomponents/ResultSection';
+import { TipsAndInterpretation } from './subcomponents/TipsAndInterperetation';
 
 const InstructionsGroup = ({ text, array, id }) => (
   <div id={id}>
@@ -279,139 +281,17 @@ export default function PhysicalFitnessTest({
           className="flex flex-col space-y-5"
         >
           {/* right side container*/}
-          <div
-            id="results"
-            className="p-5 mt-5 mb-5 overflow-x-hidden border-2 border-black rounded-2xl relative font-content lg:p-10 lg:mb-5 lg:mt-0"
-          >
-            <div
-              id="heading"
-              className="flex flex-row  justify-between items-center"
-            >
-              <h1 className="text-3xl font-bold mb-3">Results</h1>
-              <hr className="font-md text-2xl w-[50%] border-1 border-black -mr-10 mb-1" />
-            </div>
-            <hr className="w-[50%] border-1 border-black" />
-            <div id="data" className="flex flex-col relative ml-3">
-              <h2 className="font-semibold text-lg pointer-events-none">
-                {testName}
-              </h2>
-              <div
-                id="inputs"
-                className="ml-2 grid grid-cols-2 divide-x divide-black"
-              >
-                {['Reps', 'Sets', 'Time Started', 'Time End'].map(
-                  (label, index) => (
-                    <Fragment key={`${index} ${label}`}>
-                      <label
-                        htmlFor={label}
-                        className="p-1 lg:text-lg md:text-sm sm:text-xs"
-                      >
-                        {label}:{' '}
-                      </label>
-                      <input
-                        onChange={(e) => {
-                          let value = e.target.value;
-
-                          if (
-                            label !== 'Time Started' &&
-                            label !== 'Time End'
-                          ) {
-                            value = Math.max(-1, Math.min(999, value));
-                          }
-                          handleResultChange(label, value);
-                        }}
-                        type={
-                          label === 'Time Started' || label === 'Time End'
-                            ? 'time'
-                            : 'number'
-                        }
-                        min={
-                          label === 'Time Started'
-                            ? ''
-                            : label === 'Time End'
-                            ? currentTime
-                            : '-1'
-                        }
-                        max={
-                          label !== 'Time Started' && label !== 'Time End'
-                            ? '999'
-                            : undefined
-                        }
-                        value={
-                          label === 'Reps'
-                            ? testResults.reps
-                            : label === 'Sets'
-                            ? testResults.sets
-                            : label === 'Time Started'
-                            ? testResults.timeStarted
-                            : testResults.timeEnded
-                        }
-                        disabled={label === 'Time Started'}
-                        name={label}
-                        id={label}
-                        className="w-[85%] place-self-center h-fit border-1 border-black text-center font-content rounded-sm lg:px-2 "
-                      />
-                    </Fragment>
-                  ),
-                )}
-              </div>
-              <hr className="absolute -bottom-3 right-0 w-[35%] border-1 border-black" />
-            </div>
-            <div
-              id="button"
-              className="flex flex-row items-center justify-between"
-            >
-              <hr className="-ml-10 border-1 border-black w-[50%]" />
-              <button
-                onClick={() => handleSubmit()}
-                type="button"
-                className="border-1 border-black rounded-md px-5 py-1 mt-5 text-sm bg-white hover:brightness-95"
-              >
-                Submit
-              </button>
-            </div>
-          </div>
-          <div
-            id="interpretation-and-tips"
-            className="border-2 p-5 border-black rounded-2xl relative lg:p-10 overflow-x-hidden"
-          >
-            <div
-              id="heading"
-              className="flex flex-row items-center justify-between"
-            >
-              <h1 className="text-3xl font-md mb-3 font-bold font-content pr-2">
-                Interpretation
-              </h1>
-              <hr className="w-[50%] border-1 border-black -mr-10" />
-            </div>
-            <hr className="w-[50%] border-1 border-black mb-1" />
-            <div id="interpretation" className="mb-3">
-              <h2 className="font-semibold text-lg font-content">{testName}</h2>
-              <p className="ml-2 font-bold text-lg font-content">
-                {testResults.reps} : {testResults.classification}
-              </p>
-            </div>
-            <div id="tips" className="pb-10">
-              <h2 className="font-content text-sm font-semibold">
-                Tips to Improve
-              </h2>
-              <ul className="list-decimal ml-6 font-content text-sm font-medium">
-                <li>
-                  Lorem ipsum dolor sit amet consectetur. Sed augue ultrices
-                  phasellus mi nulla nisi sollicitudin sagittis.
-                </li>
-                <li>
-                  Pharetra tellus pellentesque faucibus fusce eget sagittis.
-                  Cursus sed gravida pellentesque quam.
-                </li>
-                <li>
-                  Eget justo sit tortor amet in eu diam velit. Id facilisi metus
-                  in fames faucibus viverra ullamcorper bibendum.
-                </li>
-              </ul>
-            </div>
-            <hr className="-ml-10 border-1 border-black w-[50%]" />
-          </div>
+          <ResultSection
+            testName={testName}
+            handleResultChange={handleResultChange}
+            handleSubmit={handleSubmit}
+            testResults={testResults}
+            currentTime={currentTime}
+          />
+          <TipsAndInterpretation
+            testName={testName}
+            testResults={testResults}
+          />
         </div>
       </div>
     </div>
