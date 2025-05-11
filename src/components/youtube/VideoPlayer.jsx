d2120a0244e88d3de558740ac030c5b3e1a390b0
charles-togle <149758871+charles-togle@users.noreply.github.com>
2025-05-11 11:46:40 +0800

    Youtube functional not yet designed

---
+++ src/components/youtube/VideoPlayer.jsx
@@ -0,0 +1,29 @@
+export default function VideoPlayer ({ video }) {
+  if (!video) {
+    return <div>Search and select a video to play</div>;
+  }
+
+  const videoId = video.id.videoId;
+  const url = `https://www.youtube.com/embed/${videoId}`;
+
+  return (
+    <div className='bg-accent-dark-blue p-4 rounded-lg w-full'>
+      <div className='video-player mb-4'>
+        <iframe
+          title='YouTube video player'
+          src={url}
+          width='100%'
+          height='500'
+          frameBorder='0'
+          allow='autoplay; encrypted-media'
+          allowFullScreen
+          className='rounded-md'
+        />
+      </div>
+      <h2 className='text-xl font-bold font-heading mb-2 text-primary-yellow'>
+        {video.snippet.title}
+      </h2>
+      <p className='text-content font-content'>{video.snippet.description}</p>
+    </div>
+  );
+}
