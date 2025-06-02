0f60ee17d44371d21638287af17e3a4ecf70cf40
charles-togle <149758871+charles-togle@users.noreply.github.com>
2025-06-02 19:17:33 +0800

    moved components to respective folders

--- src/services/Calculations.js
+++ src/services/Calculations.js
@@ -4,7 +4,7 @@ const convertFtToM = number => number * 0.3048;
 const convertMToFt = number => number / 0.3048;
 
 export const getBMI = (height, weight, heightUnit, weightUnit) => {
-  const heightInMeters = heightUnit === 'ft' ? convertFtToM(height) : height;
+  const heightInMeters = heightUnit === 'ft' ? convertFtToM(height) : height; // if weight unit is lbs, convert, else retain
   const weightInKg = weightUnit === 'lbs' ? convertLbsToKg(weight) : weight;
   return weightInKg / (heightInMeters * heightInMeters);
 };
