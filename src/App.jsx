338a05c22b79984215369c6db33b03679d2924f2
Charles Nathaniel Togle <149758871+charles-togle@users.noreply.github.com>
2025-06-20 08:58:22 +0800

    Resolved 4th test charles (#90)
    * Refactor QuizDashboard to use consistent JSX style
    
    Updated QuizDashboard.jsx to use single quotes for JSX attributes, arrow function style for components, and improved code consistency. No functional changes were made; this is a style and readability refactor. Fixed main menu scroll
    
    * added comment for quiz
    
    * Resolved 4th test

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function SidebarLayout () {
 
     if (currentScroll > lastScrollY && currentScroll > 200) {
       setShowMenu(false);
-    } else if (currentScroll < lastScrollY && currentScroll > 400) {
+    } else if (currentScroll < lastScrollY > 400 || currentScroll <= 0) {
       setShowMenu(true);
     }
 
--- src/components/dashboard/QuizScoreTable.jsx
+++ src/components/dashboard/QuizScoreTable.jsx
@@ -15,16 +15,19 @@ export default function QuizScoreTable ({ quizData, quizCount }) {
   }, [quizData]);
 
   return (
-    <Container className='p-0!'>
-      <table className='w-full font-content overflow-hidden mt-5 mb-5'>
+    <Container className='p-0! mb-3'>
+      <table className='w-full font-content overflow-hidden'>
         <tbody>
-          <tr className='bg-neutral-dark-blue text-white h-15 w-full'>
+          <tr className='bg-neutral-dark-blue text-white h-15 w-full text-sm md:text-base lg:text-base'>
             <th className='w-1/3'>Quiz No.</th>
             <th className='w-1/3'>Date Taken</th>
             <th className='w-1/3'>Score</th>
           </tr>
           {sortedQuizData.map((data, index) => (
-            <tr key={data.quiz_id || index} className='font-semibold h-10'>
+            <tr
+              key={data.quiz_id || index}
+              className='font-semibold h-10 text-xs lg:text-base md:text-base'
+            >
               <td>Quiz No.{data.quiz_id}</td>
               <td
                 className={
