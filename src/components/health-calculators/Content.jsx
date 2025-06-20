67928ce50739dda96d240bd820d120c872aba28c
charles-togle <149758871+charles-togle@users.noreply.github.com>
2025-06-20 21:20:53 +0800

    fix overlow problem for links

--- src/components/health-calculators/Content.jsx
+++ src/components/health-calculators/Content.jsx
@@ -1,11 +1,13 @@
 export default function Content ({ title, content }) {
   return (
-    <div className='w-full flex flex-col'>
+    <div className='w-full flex flex-col text-wrap'>
       <h2 className='text-2xl text-primary-blue font-content font-bold'>
         {title}:
       </h2>
       <hr className='h-0 w-60 border-1 border-primary-yellow mt-2 mb-4' />
-      <p className='font-content text-sm text-wrap md:text-lg'>{content}</p>
+      <div className='font-content text-sm text-wrap md:text-lg [&_a]:inline-block [&_a]:max-w-full [&_a]:truncate [&_a]:align-top'>
+        {content}
+      </div>
     </div>
   );
 }
